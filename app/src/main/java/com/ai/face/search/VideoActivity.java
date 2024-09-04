30994c5566a86ee92553b32294935cfb4a921e56
sytmrrow <3304652638@qq.com>
2024-09-05 02:46:42 +0800

    可行代碼1-沒有頁面優化

---
+++ app/src/main/java/com/ai/face/search/VideoActivity.java
@@ -0,0 +1,14 @@
+package com.ai.face.search;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.ai.face.R;
+
+public class VideoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vedio);
+    }
+}
